937b2a4f2daf2e392e2d9fcf1fcea76cfcd5c8e1
liiir1985 <liiir1985@users.noreply.github.com>
2025-10-18 14:35:23 +0800

    Improved training dataset to do both pretrain and sft

--- Qwen-SoVITS/data/qwen_t2s_dataset.py
+++ Qwen-SoVITS/data/qwen_t2s_dataset.py
@@ -10,18 +10,6 @@ import glob
 LOCAL_MODEL_PATH = "./pretrained_models/qwen3" 
 LOCAL_PHONEME_TOKENIZER = "./pretrained_models/phoneme_tokenizer/phoneme_tokenizer.json"
 
-def add_ending_punctuation_by_lang(text:str, lang:str)->str:
-    if lang == 'ja' or lang =='zh':
-        if text[-1] != "。":
-            return text + "。"
-        else:
-            return text
-    else:
-        if text[-1] != ".":        
-            return text + "."
-        else:
-            return text
-
 class Qwen3Text2SemanticDataset(Dataset):
     """dataset class for text tokens to semantic model training."""
     dataset:list
@@ -55,16 +43,22 @@ class Qwen3Text2SemanticDataset(Dataset):
             with open(i, 'r', encoding='utf-8') as f:
                 for line in f:
                     arr = line.split("\t")
-                    #prompt = f"<|im_start|>user\n文字转语音任务：{{{arr[0]}}}<|im_end|>\n<|im_start|>assistant\n"
-                    #txt_ids = tokenizer([prompt], return_tensors="pt").to('cpu')
-                    #txt_ids = txt_ids.data['input_ids'].flatten()
-                    ph_ids = torch.tensor(self.phoneme_tokenizer.encode(arr[2].replace("UNK","#")).ids, dtype=torch.long).to('cpu') + self.ph_token_start
+                    prompt = f"<|im_start|>user\n文字转语音任务：{{{arr[0]}}}<|im_end|>\n<|im_start|>assistant\n"
+                    input_ids = tokenizer([prompt], return_tensors="pt").to('cpu')
+                    input_ids = input_ids.data['input_ids'].flatten()
+                    ph_ids = torch.tensor(self.phoneme_tokenizer.encode(arr[2]).ids, dtype=torch.long).to('cpu') + self.ph_token_start
                     txt_ids = torch.cat([think_start, ph_ids, think_end], dim=0)
+                    txt_ids_full = torch.cat([input_ids, think_start, ph_ids, think_end], dim=0)
+
+                    #prompt_str = self.tokenizer.decode(txt_ids_full, skip_special_tokens=False)
+                    #print(prompt_str)
+                    
                     buffer = base64.b64decode(arr[3])
                     semantic_np = np.frombuffer(buffer, dtype=np.int16).copy()
                     semantic_ids = torch.from_numpy(semantic_np).to(torch.int64)
                     semantic_ids = semantic_ids + self.t2s_token_start
                     final = torch.cat([txt_ids, semantic_ids, eos], dim=0)
+                    final_full = torch.cat([txt_ids_full, semantic_ids, eos], dim=0)
                     #attention_mask = (final != tokenizer.pad_token_id).long()
                     # Create labels (copy of input_ids)
                     #labels = final.clone()
@@ -77,7 +71,7 @@ class Qwen3Text2SemanticDataset(Dataset):
                         max_token_cnt = tokenCnt
                         #max_line = f"({line})({i})"
                     self.dataset.append({
-                        "input_ids": final, "prompt_len": txt_ids.shape[0], "lang": arr[1]
+                        "input_ids": final, "input_ids_full":final_full, "prompt_len": txt_ids.shape[0], "prompt_len_full": txt_ids_full.shape[0], "lang": arr[1]
                     })
             f_cnt+=1
         
@@ -91,7 +85,18 @@ class Qwen3Text2SemanticDataset(Dataset):
         return self.dataset[idx]
     
     def collate(self, batch: list[dict]) -> dict:
-        input_ids_list = [b["input_ids"] for b in batch]
+        should_sft = random.randrange(100) < 10
+        should_phoneme_sft = not should_sft and random.randrange(100) < 30
+        if (not should_sft) and (not should_phoneme_sft):
+            rn = random.randrange(3)
+            if rn == 0:
+                input_ids_list = [b["input_ids"][1:b["prompt_len"]-1] for b in batch]
+            elif rn == 1:
+                input_ids_list = [b["input_ids"][b["prompt_len"]:] for b in batch]
+            else:
+                input_ids_list = [b["input_ids"] for b in batch]
+        else:
+            input_ids_list = [b["input_ids_full"] if should_sft else b["input_ids"] for b in batch]
         input_ids = torch.nn.utils.rnn.pad_sequence(input_ids_list, batch_first=True, padding_value=self.tokenizer.pad_token_id)
         
         attention_mask = torch.zeros(input_ids.shape, dtype=torch.long)
@@ -99,13 +104,15 @@ class Qwen3Text2SemanticDataset(Dataset):
         labels = input_ids.clone()
         # Mask out prompt part (all tokens up to and including "### Response:\n")
         for i, b in enumerate(batch):
-            prompt_len = b["prompt_len"]  # length of prompt in tokens
-            total_len = b["input_ids"].shape[0]
-            if self.random_mask_semantic and random.randrange(100) < 50:
-                random_semantic = random.randrange(int((input_ids_list[i].shape[0] - prompt_len) / 1.5))
-            else:
-                random_semantic = 0
-            labels[i, :prompt_len + random_semantic] = -100  # ignore prompt tokens in loss
+            prompt_len = b["prompt_len_full"] if should_sft else b["prompt_len"] # length of prompt in tokens
+            total_len = input_ids_list[i].shape[0]           
+            if should_sft or should_phoneme_sft:
+                if self.random_mask_semantic and random.randrange(100) < 50:
+                    random_semantic = random.randrange(int((input_ids_list[i].shape[0] - prompt_len) / 1.5))
+                else:
+                    random_semantic = 0
+                labels[i, :prompt_len + random_semantic] = -100  # ignore prompt tokens in loss
+                
             labels[i, total_len:] = -100
             attention_mask[i, :total_len] = 1
         return {"input_ids": input_ids, "attention_mask": attention_mask, "labels": labels}
